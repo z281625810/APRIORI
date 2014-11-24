821988948f97d1a7bb1af3b9cd1d883c601f785b
pappacurds <alexander.acebo@mymail.champlain.edu>
2014-11-23 21:13:17 -0500

    Project Created (Xcode)
    LinkedList source added

---
+++ apriori/apriori/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  apriori
+//
+//  Created by Alexander Acebo and Sun Feng on 11/23/14.
+//  Copyright (c) 2014 Alexander Acebo, Sun Feng. All rights reserved.
+//
+
+#include "LinkedList.h"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
